c3227baaf033dbf99d7167069e96278e203e0a86
Tilegen <47211384+Bakitov-Tilegen@users.noreply.github.com>
2020-10-25 12:45:46 +0500

    Корректировки пока что без JS

---
+++ js/apiMaps.js
@@ -0,0 +1,40 @@
+// function initMap() {
+//     let coordinates = { lat:59.938769, lng:30.323045 },
+//     // popupMapContent = '<div class="feedback-wrapper"></div>',
+//     map = new google.maps.Map(document.getElementById('map'), {
+//     center:coordinates}),
+
+//     marker = new google.maps.Marker({
+//         position: coordinates,
+//         map: map,
+//         // animation: google.maps.Animatoin.BOUNCE
+//     });
+// } 
+
+// image = '../img/main/map-icecream.png'
+// marker = new google.maps.Marker ({
+//     position: coordinates,
+//     map: map,
+//     icon: image
+// });
+
+
+// let map = new google.maps.Map(document.getElementById('map'),
+// {
+
+// });
+
+function initMap() {
+    let pos = {lat:59.938769, lng:30.323045 };
+    let opt = {
+        center:pos,
+        zoom:20,
+    }
+    let myMap = new google.maps.Map(document.getElementById('map'), opt);
+}
+
+// let marker = new google.maps.Marker({
+//   position: pos,
+//   map:myMap, 
+//   title:"You hover me"
+// });
